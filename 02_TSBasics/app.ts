b066c91c613a29a333bc94f04c3a2d114cb2fa6c
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-08-26 09:26:50 +0300

    Tuple

--- 02_TSBasics/app.ts
+++ 02_TSBasics/app.ts
@@ -1,8 +1,18 @@
-const person: {name: string, age: number, hobbies: string[]} = {
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
   name: 'George',
   age: 24,
   hobbies: ['Sports, Cooking'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin'); // Tupes allows to push elements in them :(
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 // let favoriteActivities: any[];
