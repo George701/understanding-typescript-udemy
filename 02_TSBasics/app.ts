95ab54d16641c38c50ec5db362053e7da00a6cab
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-08-27 09:48:21 +0300

    Literal Types

--- 02_TSBasics/app.ts
+++ 02_TSBasics/app.ts
@@ -1,17 +1,22 @@
 const combine = (
   input1: number | string,
-  input2: number | string
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
 ): number | string => {
-  if (
-    typeof input1 === 'number' &&
-    typeof input2 === 'number'
-  ) return input1 + input2;
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') result = +input1 + +input2;
+  else result = input1.toString() + input2.toString();
 
-  return input1.toString() + input2.toString();
+  return result;
+  // if (resultConversion === 'as-number') return +result;
+  // return result.toString();
 };
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Max', 'Nika');
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Max', 'Nika', 'as-text');
 console.log(combineNames);
