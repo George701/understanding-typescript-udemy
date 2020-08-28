0d56c0609f9b016bd28b5b01539eeb49ae9fcfd0
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-08-28 08:56:21 +0300

    Alias type / custom types

--- 02_TSBasics/app.ts
+++ 02_TSBasics/app.ts
@@ -1,8 +1,17 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+type User = {
+  name: string,
+  age: number
+};
+
+
 const combine = (
-  input1: number | string,
-  input2: number | string,
-  resultConversion: 'as-number' | 'as-text'
-): number | string => {
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+): Combinable => {
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') result = +input1 + +input2;
   else result = input1.toString() + input2.toString();
