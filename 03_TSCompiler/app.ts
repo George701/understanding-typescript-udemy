23cbff48a3ba758e7533b4d00995d44d46562d59
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-09-07 09:35:41 +0300

    lib

--- 03_TSCompiler/app.ts
+++ 03_TSCompiler/app.ts
@@ -2,3 +2,9 @@
 let num: number;
 num = 5;
 console.log('Code goes here!');
+
+const btn = document.querySelector('button')!;
+
+btn.addEventListener('click', () => {
+  console.log('Clicked!');
+})
