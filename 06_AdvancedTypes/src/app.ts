662ce708342e43800a2bedd1b02bf446d9867f11
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-10-29 09:37:19 +0300

    Discriminated unions

--- 06_AdvancedTypes/src/app.ts
+++ 06_AdvancedTypes/src/app.ts
@@ -73,3 +73,31 @@ function useVehicle(v: Vehicle){
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated union - one common property for class/interface
+interface Bird {
+  type: 'bird',
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse',
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed: number;
+  switch(animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log('Moving with speed: ' + animal);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
