d3465e66b90f9e546e76b88b89c36667b9bc9658
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-11-03 08:35:23 +0300

    function overloads

--- 06_AdvancedTypes/src/app.ts
+++ 06_AdvancedTypes/src/app.ts
@@ -23,12 +23,22 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+// Function overloads
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
 function add(a: Combinable, b: Combinable){
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const resultOne = add(1,5);
+// const resultTwo = add('Walter', 'White') as string;
+const resultTwo = add('Walter', ' White');
+resultTwo.split(' ');
 
 type UnknownEmployee = Employee | Admin;
 
