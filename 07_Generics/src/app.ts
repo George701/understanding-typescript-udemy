670d6149d62155f7d7595ae6d583a83ed7879952
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-11-09 10:32:16 +0300

    Generic functions

--- 07_Generics/src/app.ts
+++ 07_Generics/src/app.ts
@@ -3,8 +3,20 @@ const names: Array<string> = ['Max', 'Manu'];
 // const names: string[] = ['Max', 'Manu']
 names[0].split('');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done')
-  }, 2000)
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done')
+//   }, 2000)
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return {
+    ...objA,
+    ...objB,
+  }
+};
+
+const merged = merge({name: 'Max', hobbies: ['sports']}, {age: 30})
+console.log(merged);
+console.log(merged.name);
+console.log(merged.age);
