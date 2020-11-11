74f900d258e581b3e4c39ad72027564c416aba00
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-11-11 09:34:53 +0300

    keyof constraint

--- 07_Generics/src/app.ts
+++ 07_Generics/src/app.ts
@@ -33,6 +33,12 @@ function countAndDescribe<T extends Lengthly>(element: T): [T, string] {
     descriptionText = 'Got ' + element.length + ' elements';
   }
   return [element, descriptionText]
-}
+};
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+};
+
+extractAndConvert({ name: 'Max' }, 'name');
 
 
