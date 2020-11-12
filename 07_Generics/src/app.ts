26b0ac922b73059987300d5b6fd320ef430d19ac
Georgii Chernikov <georgii.chernikov@valamis.com>
2020-11-12 09:55:43 +0300

    generic objects

--- 07_Generics/src/app.ts
+++ 07_Generics/src/app.ts
@@ -41,4 +41,36 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 
 extractAndConvert({ name: 'Max' }, 'name');
 
+// beter usage for primitive types
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getData() {
+    return [...this.data];
+  }
+};
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.addItem('Jose');
+textStorage.removeItem('Max');
+console.log(textStorage.getData());
+
+// const objStorage = new DataStorage<object>();
+// const objManu = { name: 'Manu'};
+// objStorage.addItem({ name: 'Max'});
+// objStorage.addItem(objManu);
+// objStorage.addItem({ name: 'Jose'});
+// objStorage.removeItem(objManu);
+// console.log(objStorage.getData());
 
