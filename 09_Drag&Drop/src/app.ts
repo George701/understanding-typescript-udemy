aa0d72d7b2bf8dec24d90f63f3c5f715fa4d55a2
Georgii Chernikov <georgii.chernikov@valamis.com>
2021-04-15 16:49:32 +0300

    Fetching user input

--- 09_Drag&Drop/src/app.ts
+++ 09_Drag&Drop/src/app.ts
@@ -39,12 +39,47 @@ class ProjectInput {
 
     this.configure();
     this.attach();
+  };
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, try again!');
+      return;
+    } else {
+      return [
+        enteredTitle,
+        enteredDescription,
+        +enteredPeople
+      ];
+    }
   }
 
   @autoBind
   private submitHandler (event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    // console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log('title', title);
+      console.log('desc', desc);
+      console.log('people', people);
+      this.clearInputs();
+    }
   }
 
   private configure () {
