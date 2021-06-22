62013f7e8d0d92c1e1484c33f8ed23eb9f54ba58
Georgii Chernikov <georgii.chernikov@valamis.com>
2021-06-22 10:03:22 +0300

    Add project list class

--- 09_Drag&Drop/src/app.ts
+++ 09_Drag&Drop/src/app.ts
@@ -66,6 +66,34 @@ function autoBind(
   return adjDescriptor;
 };
 
+// Project List
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${type}-projects`;
+    this.attach();
+    this.renderContent();
+  };
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  };
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  };
+};
+
 // Project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -162,3 +190,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const FinishedProjectList = new ProjectList('finished');
